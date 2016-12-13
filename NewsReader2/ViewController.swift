f2506cff4f44bfb025dd7b9440f322611b740952
Nori Ohara <hideno0110@gmail.com>
2016-12-13 13:03:37 +0900

    Initial Commit

---
+++ NewsReader2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewsReader2
+//
+//  Created by 大原秀典 on 2016/12/13.
+//  Copyright © 2016年 大原秀典. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
