eac680b5df57cb295dff19e4f47b50ba7ed2a75a
Nori Ohara <hideno0110@gmail.com>
2016-12-13 14:28:08 +0900

    first commit

---
+++ NewsReader2/DetailViewController.swift
@@ -0,0 +1,9 @@
+//
+//  DetailViewController.swift
+//  NewsReader2
+//
+//  Created by 大原秀典 on 2016/12/13.
+//  Copyright © 2016年 大原秀典. All rights reserved.
+//
+
+import Foundation
---
+++ NewsReader2/Item.swift
@@ -0,0 +1,9 @@
+//
+//  Item.swift
+//  NewsReader2
+//
+//  Created by 大原秀典 on 2016/12/13.
+//  Copyright © 2016年 大原秀典. All rights reserved.
+//
+
+import Foundation
